a0a4ccc41382fe8b5902d983830405f409b84482
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2021-11-30 15:27:32 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// let button = document.getElementById("btn");
+
+// button.onclick = function(){
+   
+//     return  alert("So when are you coming back?")
+//     const numbers = [2,3,4,5,6,7,8,9]
+//    const input = document.getElementById("num1").value;
+//    const inputResult = parseInt(num1)
+//     let y = numbers1.reduce((a,b)=>a+b) / numbers1.length;    
+          
+    
+//      }
+
+// document.querySelector("button").addEventListener("click", function(){
+//     alert ("Haleluyah");
+// });
+     
+    
+//  handleClick () 
+    
+
+
+           
+            
